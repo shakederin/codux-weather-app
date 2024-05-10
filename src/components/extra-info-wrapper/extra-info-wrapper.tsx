17b8b91498aabf690ae38553a5d6591bc99fbd3c
Shaked Erinberg <shakede@wix.com>
2024-05-10 18:40:10 +0300

    fix extrainfo svgs

--- src/components/extra-info-wrapper/extra-info-wrapper.tsx
+++ src/components/extra-info-wrapper/extra-info-wrapper.tsx
@@ -2,9 +2,9 @@ import '../../styles/common/style-guide.css';
 import classNames from 'classnames';
 import styles from './extra-info-wrapper.module.scss';
 import { ExtraInfo } from '../extra-info/extra-info';
-import thermometer from '../../assets/thermometer.png';
-import drop from '../../assets/drop.svg';
-import wind from '../../assets/wind.png';
+import { ReactComponent as WindSvg } from '../../assets/wind.svg';
+import { ReactComponent as ThermometerSvg } from '../../assets/thermometer.svg';
+import { ReactComponent as DropSvg } from '../../assets/drop.svg';
 
 export interface ExtraInfoWrapperProps {
     data: {
@@ -22,20 +22,17 @@ export interface ExtraInfoWrapperProps {
 export const ExtraInfoWrapper = ({ data }: ExtraInfoWrapperProps) => {
     return (
         <div className={classNames(styles.root)}>
-            <ExtraInfo
-                className={'gradientBottom'}
-                data={{ type: 'Wind', value: data.Wind }}
-                icon={wind}
-            />
+            <ExtraInfo className={'gradientBottom'} data={{ type: 'Wind', value: data.Wind }} icon={<WindSvg className={'white-svg'}/>}/>
             <ExtraInfo
                 className={'gradientTop'}
                 data={{ type: 'Pressure', value: data.Pressure }}
-                icon={thermometer}
+                icon={<ThermometerSvg className={'white-svg'}/>}
             />
             <ExtraInfo
                 className={'purpleGradientRight'}
                 data={{ type: 'Humidity', value: data.Humidity }}
-                icon={drop}
+                icon={<DropSvg className={'white-svg'}/>}
+
             />
         </div>
     );
--- src/components/extra-info/extra-info.tsx
+++ src/components/extra-info/extra-info.tsx
@@ -5,7 +5,7 @@ import styles from './extra-info.module.scss';
 export interface ExtraInfoProps {
     className?: string;
     data: { type: string; value: string };
-    icon: string;
+    icon: JSX.Element
 }
 
 /**
@@ -13,9 +13,10 @@ export interface ExtraInfoProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const ExtraInfo = ({ className, data, icon }: ExtraInfoProps) => {
+
     return (
         <div className={classNames(styles.root, className)}>
-            <img src={icon} className={styles['info-icon']} />
+            {icon}
             <div >{data.type}</div>
             <div>{data.value}</div>
         </div>
