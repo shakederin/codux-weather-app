ee0e5f3878d03a96f09c12718742230793498173
Shaked Erinberg <shakede@wix.com>
2024-05-10 19:00:12 +0300

    fix main board

--- src/_codux/boards/main/main.board.tsx
+++ src/_codux/boards/main/main.board.tsx
@@ -3,7 +3,15 @@ import { Main } from '../../../components/main/main';
 
 export default createBoard({
     name: 'Main',
-    Board: () => <Main />,
+    Board: () => (
+        <Main
+            data={{
+                    date: '25 January, Wednesday',
+                    realFeel: 10,
+                    temp: -18,
+                }}
+        />
+    ),
     isSnippet: true,
     environmentProps: {
         windowWidth: 430,
